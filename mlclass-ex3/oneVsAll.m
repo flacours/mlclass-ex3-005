0674250856bd39aac33fe66a38277250f8c194d1
Francois Lacoursiere <flacours@gmail.com>
2014-04-03 13:01:00 -0400

    p2

--- mlclass-ex3/oneVsAll.m
+++ mlclass-ex3/oneVsAll.m
@@ -49,7 +49,27 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+%disp(size(all_theta));
+for( k=1 : num_labels)
+   theta = all_theta(k,:);
+   
 
+   % Set Initial theta
+   initial_theta = zeros(n + 1, 1);
+     
+   % Set options for fminunc
+   options = optimset('GradObj', 'on', 'MaxIter', 50);
+ 
+   % Run fmincg to obtain the optimal theta
+   % This function will return theta and the cost 
+   [theta] = ...
+   fmincg (@(t)(lrCostFunction(t, X, (y == k), lambda)), ...
+           initial_theta, options);
+
+   all_theta(k,:) = theta;
+%   disp(size(theta));
+
+end
 
 
 
